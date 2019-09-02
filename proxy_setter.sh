d417a1c0a4e40ec55bb1363bda53f9f727043a17
iit2018110 <46761014+iit2018110@users.noreply.github.com>
2019-09-03 01:52:30 +0530

    Add files via upload

---
+++ proxy_setter.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+protocols=(http https ftp socks)
+
+cd 
+cd /etc/apt
+
+touch newfile.txt
+
+echo "enter proxy"
+read proxy
+
+echo "enter port"
+read port
+
+echo "enter username"
+read username
+
+echo "enter password -----Note: use %40 in place of @"
+read -s password
+
+
+for p in "${protocols[@]}";
+do
+	echo "Acquire::$p::proxy \"$p://$username:$password@$proxy:$port/\";" >> newfile.txt
+done
+
+cat newfile.txt > apt.conf
+
+rm newfile.txt
+
+echo "Yeah! proxy setup completed"
+
